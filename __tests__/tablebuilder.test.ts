bc4f4be2fe036ad8346bcf473e6aaa25b4732ba9
yamilmedina <yamilmedina@users.noreply.github.com>
2024-04-30 09:48:05 +0200

    chore: github actions standard template-like, more tests

--- __tests__/tablebuilder.test.ts
+++ __tests__/tablebuilder.test.ts
@@ -1,7 +1,7 @@
 import * as tablebuild from '../src/tablebuilder'
 import { Benchmark } from '../src/benchmark'
 import { debug } from '@actions/core'
-import sample from './sample.json'
+import sample from './fixtures/sample.json'
 
 const runMock = jest.spyOn(tablebuild, 'buildBenchmarkTable')
 
@@ -10,8 +10,8 @@ describe('tablebuilder', () => {
         let benchmarks: Benchmark[] = JSON.parse(JSON.stringify(sample))
         debug("Num of benchmarks: " + benchmarks.length)
         let result = tablebuild.buildBenchmarkTable(benchmarks)
-        expect(result).toBe(expectedTable)
+        expect(result).toBe(expectedOutputTable)
     })
 })
 
-const expectedTable = "<table><tr><th>Benchmark</th><th>Score</th><th>Error</th></tr><tr><td>com.benchmarks.MessagesBenchmark.messageInsertionBenchmark</td><td>1.782ops/s</td><td>0.223ops/s</td></tr><tr><td>com.benchmarks.MessagesBenchmark.queryMessagesBenchmark</td><td>50.457ops/s</td><td>3.394ops/s</td></tr></table>"
+const expectedOutputTable = "<table><tr><th>Benchmark</th><th>Score</th><th>Error</th></tr><tr><td>com.benchmarks.MessagesBenchmark.messageInsertionBenchmark</td><td>1.782ops/s</td><td>0.223ops/s</td></tr><tr><td>com.benchmarks.MessagesBenchmark.queryMessagesBenchmark</td><td>50.457ops/s</td><td>3.394ops/s</td></tr></table>"
